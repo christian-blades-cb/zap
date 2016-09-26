eeb2126e99d8cdfdd500dd82c92dfb8f4822c1b1
Christian Blades <cblades@pindropsecurity.com>
2016-09-25 21:38:41 -0400

    logfmt -- not quite working

---
+++ logfmt_encoder.go
@@ -0,0 +1,241 @@
+package zap
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"strconv"
+	"sync"
+	"time"
+	"unicode/utf8"
+)
+
+var ErrAddObjNotImplemented = errors.New("AddObject is not implemented for logfmt encoder. Please consider implementing the LogMarshaler interface.")
+
+var logfmtPool = sync.Pool{
+	New: func() interface{} {
+		return &logfmtEncoder{
+			bytes: make([]byte, 0, _initialBufSize),
+		}
+	},
+}
+
+type logfmtEncoder struct {
+	bytes []byte
+}
+
+func (enc *logfmtEncoder) truncate() {
+	enc.bytes = enc.bytes[:0]
+}
+
+func newLogfmtEncoder() *logfmtEncoder {
+	enc := logfmtPool.Get().(*logfmtEncoder)
+	enc.truncate()
+	return enc
+}
+
+// Clone copies the current encoder, including any data already encoded
+func (enc *logfmtEncoder) Clone() Encoder {
+	clone := logfmtPool.Get().(*logfmtEncoder)
+	clone.truncate()
+	clone.bytes = append(clone.bytes, enc.bytes...)
+	return clone
+}
+
+func (enc *logfmtEncoder) Free() {
+	logfmtPool.Put(enc)
+}
+
+// WriteEntry writes a complete log message to the supplied writer, including
+// the encoder's accumulated fields. It doesn't modify or lock the encoder's
+// underlying byte slice. It's safe to call from multiple goroutines, but it's
+// not safe to call WriteEntry while adding fields.
+func (enc *logfmtEncoder) WriteEntry(sink io.Writer, msg string, lvl Level, t time.Time) error {
+	final := logfmtPool.Get().(*logfmtEncoder)
+	final.truncate()
+	final.addLevel(lvl)
+	final.AddString("msg", msg)
+	final.addTime(t)
+	final.AddString("ts", t.Format(time.RFC3339))
+	if len(enc.bytes) > 0 {
+		final.spacing()
+		final.bytes = append(final.bytes, enc.bytes...)
+	}
+	final.bytes = append(final.bytes, '\n')
+
+	expectedBytes := len(final.bytes)
+	n, err := sink.Write(final.bytes)
+	final.Free()
+	if err != nil {
+		return err
+	}
+	if n != expectedBytes {
+		return fmt.Errorf("incomplete write: only wrote %v of %v bytes", n, expectedBytes)
+	}
+	return nil
+}
+
+// https://github.com/kr/logfmt/blob/master/decode.go#L11-L20
+//
+// EBNFish:
+//
+// 	ident_byte = any byte greater than ' ', excluding '=' and '"'
+// 	string_byte = any byte excluding '"' and '\'
+// 	garbage = !ident_byte
+// 	ident = ident_byte, { ident byte }
+// 	key = ident
+// 	value = ident | '"', { string_byte | '\', '"' }, '"'
+// 	pair = key, '=', value | key, '=' | key
+// 	message = { garbage, pair }, garbage
+
+// addKey adds the key to the bytes array.
+// It ensures that the key conforms to the ident specification, replacing offending characters with the unicode replacement character (\ufffd)
+func (enc *logfmtEncoder) addKey(key string) {
+	for _, kchar := range key {
+		if kchar > ' ' && kchar != '=' && kchar != '"' { // ident_byte
+			enc.bytes = append(enc.bytes, byte(kchar))
+		} else { // garbage
+			enc.bytes = append(enc.bytes, `\ufffd`...)
+		}
+	}
+}
+
+// spacing adds a garbage byte
+func (enc *logfmtEncoder) spacing() {
+	if len(enc.bytes) == 0 {
+		return
+	}
+	enc.bytes = append(enc.bytes, ' ')
+}
+
+// AddBool appends the key and, if the value is false, the value 'false' (lone keys are interpreted as 'true'.)
+func (enc *logfmtEncoder) AddBool(key string, value bool) {
+	enc.spacing()
+	enc.addKey(key)
+	if value == false {
+		enc.bytes = append(enc.bytes, `=false`...)
+	}
+}
+
+// AddFloat64 appends the key and float64 value to the encoder's log line.
+func (enc *logfmtEncoder) AddFloat64(key string, value float64) {
+	enc.spacing()
+	enc.addKey(key)
+	enc.bytes = append(enc.bytes, '=')
+	switch {
+	case math.IsNaN(value):
+		enc.bytes = append(enc.bytes, `NaN`...)
+	case math.IsInf(value, 1):
+		enc.bytes = append(enc.bytes, `+Inf`...)
+	case math.IsInf(value, -1):
+		enc.bytes = append(enc.bytes, `-Inf`...)
+	default:
+		enc.bytes = strconv.AppendFloat(enc.bytes, value, 'f', -1, 64)
+	}
+}
+
+// AddInt appends the key and int value to the encoder's log line.
+func (enc *logfmtEncoder) AddInt(key string, value int) {
+	enc.AddInt64(key, int64(value))
+}
+
+// AddInt64 appends the key and int64 value to the encoder's log line.
+func (enc *logfmtEncoder) AddInt64(key string, value int64) {
+	enc.spacing()
+	enc.addKey(key)
+	enc.bytes = append(enc.bytes, '=')
+	enc.bytes = strconv.AppendInt(enc.bytes, value, 10)
+}
+
+// AddUInt appends the key and uint value to the encoder's log line.
+func (enc *logfmtEncoder) AddUint(key string, value uint) {
+	enc.AddUint64(key, uint64(value))
+}
+
+// AddUInt64 appends the key and uint64 value to the encoder's log line.
+func (enc *logfmtEncoder) AddUint64(key string, value uint64) {
+	enc.spacing()
+	enc.addKey(key)
+	enc.bytes = append(enc.bytes, '=')
+	enc.bytes = strconv.AppendUint(enc.bytes, value, 10)
+}
+
+// AddMarshaler adds a LogMarshaler to the encoder's fields. Since there's no real nesting in logfmt, we'll just trust the LogMarshaler to "do the right thing".
+func (enc *logfmtEncoder) AddMarshaler(key string, obj LogMarshaler) error {
+	enc.spacing()
+	return obj.MarshalLog(enc)
+}
+
+// AddObject is not implemented for LogfmtEncoder
+func (enc *logfmtEncoder) AddObject(key string, value interface{}) error {
+	enc.AddString(key, fmt.Sprintf("%+v", value))
+	return nil
+}
+
+// AddString adds a key and string value to the encoder's logline, escaping any '\' or '"' runes
+func (enc *logfmtEncoder) AddString(key, value string) {
+	enc.spacing()
+	enc.addKey(key)
+	enc.bytes = append(enc.bytes, '=', '"')
+
+	for i := 0; i < len(value); {
+		if b := value[i]; b < utf8.RuneSelf {
+			i++
+
+			switch b {
+			case '\\', '"':
+				enc.bytes = append(enc.bytes, '\\', b)
+			case '\n':
+				enc.bytes = append(enc.bytes, '\\', 'n')
+			case '\r':
+				enc.bytes = append(enc.bytes, '\\', 'r')
+			case '\t':
+				enc.bytes = append(enc.bytes, '\\', 't')
+			default:
+				enc.bytes = append(enc.bytes, b)
+			}
+
+			continue
+		}
+		c, size := utf8.DecodeRuneInString(value[i:])
+		if c == utf8.RuneError && size == 1 {
+			enc.bytes = append(enc.bytes, `\ufffd`...)
+			i++
+			continue
+		}
+		enc.bytes = append(enc.bytes, value[i:i+size]...)
+		i += size
+	}
+
+	enc.bytes = append(enc.bytes, '"')
+}
+
+func (enc *logfmtEncoder) addLevel(lvl Level) {
+	enc.spacing()
+	enc.addKey("level")
+	enc.bytes = append(enc.bytes, '=')
+	switch lvl {
+	case DebugLevel:
+		enc.bytes = append(enc.bytes, `debug`...)
+	case InfoLevel:
+		enc.bytes = append(enc.bytes, `info`...)
+	case WarnLevel:
+		enc.bytes = append(enc.bytes, `warn`...)
+	case ErrorLevel:
+		enc.bytes = append(enc.bytes, `error`...)
+	case PanicLevel:
+		enc.bytes = append(enc.bytes, `panic`...)
+	case FatalLevel:
+		enc.bytes = append(enc.bytes, `fatal`...)
+	default:
+		enc.bytes = strconv.AppendInt(enc.bytes, int64(lvl), 10)
+	}
+}
+
+func (enc *logfmtEncoder) addTime(ts time.Time) {
+	enc.spacing()
+	enc.addKey("ts")
+	enc.bytes = append(enc.bytes, '=')
+	enc.bytes = ts.AppendFormat(enc.bytes, time.RFC3339)
+}
---
+++ logfmt_encoder_test.go
@@ -0,0 +1,77 @@
+package zap
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"math"
+	"testing"
+)
+
+func TestReplacementCharValue(t *testing.T) {
+	if '\ufffd' <= ' ' {
+		t.Error("value out of ident range")
+	}
+}
+
+func withLogfmtEncoder(f func(*logfmtEncoder)) {
+	enc := newLogfmtEncoder()
+	f(enc)
+	enc.Free()
+}
+
+func assertLogfmtOutput(t *testing.T, desc, expected string, f func(Encoder)) {
+	withLogfmtEncoder(func(enc *logfmtEncoder) {
+		f(enc)
+		assert.Equal(t, expected, string(enc.bytes), "Unexpected encoder output after adding a %s.", desc)
+	})
+	withLogfmtEncoder(func(enc *logfmtEncoder) {
+		enc.AddString("foo", "bar")
+		f(enc)
+		expectedPrefix := `foo="bar"`
+		if expected != "" {
+			expectedPrefix += " "
+		}
+
+		assert.Equal(t, expectedPrefix+expected, string(enc.bytes), "Unexpected encoder output after adding a %s as a second field.", desc)
+	})
+}
+
+func TestTextEncoderFields(t *testing.T) {
+	tests := []struct {
+		desc     string
+		expected string
+		f        func(Encoder)
+	}{
+		{"string", `k=v`, func(e Encoder) { e.AddString("k", "v") }},
+		{"string", `k="v"`, func(e Encoder) { e.AddString("k", "") }},
+		{"bool", "k", func(e Encoder) { e.AddBool("k", true) }},
+		{"bool", "k=false", func(e Encoder) { e.AddBool("k", false) }},
+		{"int", "k=42", func(e Encoder) { e.AddInt("k", 42) }},
+		{"int64", "k=42", func(e Encoder) { e.AddInt64("k", 42) }},
+		{"int64", fmt.Sprintf("k=%d", math.MaxInt64), func(e Encoder) { e.AddInt64("k", math.MaxInt64) }},
+		{"uint", "k=42", func(e Encoder) { e.AddUint("k", 42) }},
+		{"uint64", "k=42", func(e Encoder) { e.AddUint64("k", 42) }},
+		{"uint64", fmt.Sprintf("k=%d", uint64(math.MaxUint64)), func(e Encoder) { e.AddUint64("k", math.MaxUint64) }},
+		{"float64", "k=1", func(e Encoder) { e.AddFloat64("k", 1.0) }},
+		{"float64", "k=10000000000", func(e Encoder) { e.AddFloat64("k", 1e10) }},
+		{"float64", "k=NaN", func(e Encoder) { e.AddFloat64("k", math.NaN()) }},
+		{"float64", "k=+Inf", func(e Encoder) { e.AddFloat64("k", math.Inf(1)) }},
+		{"float64", "k=-Inf", func(e Encoder) { e.AddFloat64("k", math.Inf(-1)) }},
+		{"marshaler", `loggable="yes"`, func(e Encoder) {
+			assert.NoError(t, e.AddMarshaler("k", loggable{true}), "Unexpected error calling MarshalLog.")
+		}},
+		{"marshaler", "k={}", func(e Encoder) {
+			assert.Error(t, e.AddMarshaler("k", loggable{false}), "Expected an error calling MarshalLog.")
+		}},
+		{"map[string]string", `k="map[loggable:yes]"`, func(e Encoder) {
+			assert.NoError(t, e.AddObject("k", map[string]string{"loggable": "yes"}), "Unexpected error serializing a map.")
+		}},
+		{"arbitrary object", `k="{Name:jane}"`, func(e Encoder) {
+			assert.NoError(t, e.AddObject("k", struct{ Name string }{"jane"}), "Unexpected error serializing a struct.")
+		}},
+	}
+
+	for _, tt := range tests {
+		assertLogfmtOutput(t, tt.desc, tt.expected, tt.f)
+	}
+}
